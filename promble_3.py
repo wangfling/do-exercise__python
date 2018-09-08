d8c19d69411410f7178e85bf2c3ffcfaa9b4d357
wangfling <wangfl_dhu@163.com>
2018-09-08 19:58:43 +0800

    Update promble_3.py

--- promble_3.py
+++ promble_3.py
@@ -24,3 +24,19 @@ class Solution:
                 numbers[i] = numbers[temp]
                 numbers[temp] = temp
         return False
+
+    
+#遍历
+    # -*- coding:utf-8 -*-
+class Solution:
+    # 这里要特别注意~找到任意重复的一个值并赋值到duplication[0]
+    # 函数返回True/False
+    def duplicate(self, numbers, duplication):
+        # write code here
+        for i in range(len(numbers)):
+            if numbers[i] in duplication:
+                duplication[0] = numbers[i]
+                return True
+            else:
+                duplication.append(numbers[i])
+        return False
