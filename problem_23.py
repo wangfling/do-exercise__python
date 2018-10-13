9561300cc6ad6089de964a946dce2a2dbaa5c98e
wangfling <wangfl_dhu@163.com>
2018-10-13 17:05:39 +0800

    Update problem_23.py

--- problem_23.py
+++ problem_23.py
@@ -33,5 +33,35 @@ class Solution:
             p = p.next
             q = q.next
         return p
+    
+    
+    
+# -*- coding:utf-8 -*-
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+class Solution:
+    def EntryNodeOfLoop(self, pHead):
+        # write code here
+        '''
+//先说个定理：两个指针一个fast、一个slow同时从一个链表的头部出发
+//fast一次走2步，slow一次走一步，如果该链表有环，两个指针必然在环内相遇
+//此时只需要把其中的一个指针重新指向链表头部，另一个不变（还在环内），
+//这次两个指针一次走一步，相遇的地方就是入口节点。
+//这个定理可以自己去网上看看证明。'''
+        if pHead == None or pHead.next == None:
+            return None
+        pSlow,pFast = pHead,pHead
+        while pFast.next != None:
+            pSlow = pSlow.next
+            pFast = pFast.next.next
+            if pSlow == pFast:
+                p,q = pHead,pSlow
+                while p != q:
+                    p = p.next
+                    q = q.next
+                return p
+        return None
         
                 
